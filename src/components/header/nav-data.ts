31ab9bd8afc3a949e491b031e2d55bbc7e26fe44
lucasignislab <clicknova1601@gmail.com>
2026-06-22 15:34:01 -0300

    des on

--- src/components/header/nav-data.ts
+++ src/components/header/nav-data.ts
@@ -143,6 +143,10 @@ export const NAV_LINKS: NavLink[] = [
     href: "/cases",
   },
   {
+    label: "Design System",
+    href: "/design-system",
+  },
+  {
     label: "Materiais",
     href: "#materiais",
     menu: {
